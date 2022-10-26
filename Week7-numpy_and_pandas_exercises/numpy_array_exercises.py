05fe6ca0ecf293e8567f49fb106a323ccef3b29b
Blowi <mohammedalbalawi@arizona.edu>
2022-10-25 17:58:12 -0700

    Numpy and Pandas HW Submission for Blowi818

--- Week7-numpy_and_pandas_exercises/numpy_array_exercises.py
+++ Week7-numpy_and_pandas_exercises/numpy_array_exercises.py
@@ -5,28 +5,34 @@ import numpy as np
 
 x = np.arange(0, 3**3)
 
+print(x)
+
 #%%
 # 1. What is the length of x?
 
+len(x)
 #%%
 # 2. How do you get the first value out of x?
-
+x[0]
 #%%
 # 3. How do you get the last value out of x?
-
+x[-1]
 #%%
 # 4. How do you get the first 5 values out of x?
-
+x[0:5]
 #%%
 # 5. What about the last 5 values?
-
+x[22:27]
 #%%
 # 6. How do you get every other value out of x?
-
+x[::2]
 #%%
 # 7. Get the first 9 values of x, and reshape them to a
 #    3x3 matrix. Assign this matrix to the variable `y`
-y = None
+x9 = x[0:9]
+print(x9)
+
+y = x9.reshape(3,3)
 print(y)
 
 #%%
@@ -37,17 +43,22 @@ y = np.array([
     [6, 7, 8],
 ])
 
+y[1,1]
+
 #%%
 # 9. How do you get the first row out of y?
-
+y[0,:]
 #%%
 # 10. How do you get the first column out of y?
-
+y[:,0]
 #%%
 # 11. Reshape x into a 3x3x3 cube. Assign this
 #     to the variable `z`
-z = None
+z = x.reshape((3,3,3))
 print(z.shape)
+#%%
+
+print(z)
 
 #%%
 # 12. How do you get the "center" value from z?
@@ -63,14 +74,18 @@ z = np.array([
      [24,25,26]]
 ])
 
+z[1,1,1]
+
 
 #%%
 # 13. How do you take the total sum of z?
-
+z.sum()
 #%%
 # 14. How do you take the sum along the first 
 #     dimension of z?
-
+z.sum(0)
 #%%
 # 15. How do you take the sum along the last
 #     dimension of z?
+z.sum(2)
+# %%
--- Week7-numpy_and_pandas_exercises/pandas_dataframe_exercises.py
+++ Week7-numpy_and_pandas_exercises/pandas_dataframe_exercises.py
@@ -1,6 +1,7 @@
 #%%
 # This script contains exercises on 
 # manipulating Series and DataFrames with pandas
+from enum import unique
 import urllib
 import numpy as np
 import pandas as pd
@@ -66,45 +67,59 @@ verde_df.head()
 
 # %%
 # 1. How do you see a quick summary of what is in `verde_df`?
+verde_df.describe()
 
 # %%
 # 2. How do you get a listing of the columns in `verde_df`?
-
+verde_df.info()
 # %%
 # 3. How do you select the streamflow column in `verde_df`?
-
+verde_df.streamflow
 #%%
 # 4. How do you plot the streamflow in `verde_df`?
-
+verde_df['streamflow'].plot()
+plt.ylabel('Streamflow [cfs]')
+plt.xlabel('year')
 #%%
 # 5. How do you get the last streamflow value from `verde_df`?
-
+verde_df.streamflow[-1]
 #%%
 # 6. What is the mean streamflow value for the 30 year period?
-
+verde_df['streamflow'].mean()
 #%%
 # 7. What is the maximum value for the 30 year period?
+verde_df['streamflow'].nlargest(1)
+# or this:
 
+verde_df['streamflow'].max()
+#%%
+# to know location:
+verde_df['streamflow'].idxmax()
 #%% 
 # 8. How do you find the maximum streamflow value for each year?
-
+verde_df['streamflow'].groupby(verde_df.index.year).max()
 #%%
 # 9. How do you make a scatter plot of 
 #    `dayl (s)` versus `tmax (deg c)`?
 # INFO: `dayl` is the day length in seconds and
 #       `tmax` is the daily maximum temperature
+verde_df.plot.scatter(x = 'tmax (deg c)',y = 'dayl (s)')
 
 #%%
 # 10. How do you calculate (and plot) the mean
 #     daily minimum temperature for each day of year? 
 #     And plot it?
 # INFO: Daily minimum temperature is in the column `tmin (deg c)`
+doy_mean = verde_df['tmin (deg c)'].groupby(verde_df.index.dayofyear).mean()
+doy_mean.plot()
+
 
 #%%
 # 11. What is the average value of all columns for October 10 
 #     across all years?
 # INFO: October 10 is the 283rd day of year
-
+Oct10_mean = verde_df.groupby(verde_df.index.dayofyear).mean(283)
+print(Oct10_mean)
 #%%
 # ----------------------------------------------------------------------------
 # ========== NEW DATASET =====================================================
@@ -124,16 +139,21 @@ iris_df.index = pd.Series(
 )
 iris_df.head()
 
+#%%
+
+print(iris_df)
+
 # %%
 # 12. How do you view the "unique" species in the `iris_df` index?
 
+print(iris_df['species'].unique())
 # %%
 # 13. How do you "locate" only rows for the `versicolor` species?
-
+iris_df.loc[iris_df['species']=='versicolor']
 # %%
 # 14. How do you group by the 3 different species and take 
 #     the mean across the whole dataframe?
-
+iris_df.groupby(['species']).mean()
 # %%
 # 15. How do you make a scatter plot of the `sepal length (cm)` 
 #     versus the `petal length (cm)` for the `versicolor`` species?
@@ -142,3 +162,6 @@ iris_df.head()
 #               the same figure. Color them 'tomato', 'darkcyan', 
 #               and 'darkviolet', respectively. 
 #               Worth 1 point extra credit
+
+iris_df.plot.scatter(x = 'petal length (cm)',y = 'sepal length (cm)')
+# %%
